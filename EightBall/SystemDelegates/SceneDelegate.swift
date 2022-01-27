4231c241f6606d9fbacd4d679b212d384e250dfa
Ostap Tyvonovych <ostaptyv@gmail.com>
2022-01-27 23:03:00 +0200

    Initial commit

---
+++ EightBall/SystemDelegates/SceneDelegate.swift
@@ -0,0 +1,16 @@
+//
+//  SceneDelegate.swift
+//  EightBall
+//
+//  Created by Ostap Tyvonovych on 27.01.2022.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+    var window: UIWindow?
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+    }
+}
+
